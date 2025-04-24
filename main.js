05af4b6f19514f1d4dd68030db46f250249695c1
omiduxui <oahmadnejadux@gmail.com>
2025-04-24 16:04:20 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    document.getElementById('loading-overlay').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+  });
+  
